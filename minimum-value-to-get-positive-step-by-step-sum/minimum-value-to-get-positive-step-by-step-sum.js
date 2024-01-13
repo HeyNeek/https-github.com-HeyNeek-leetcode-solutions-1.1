7a95ed06e9b972f8baf898bd3f5229eb37773ec1
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-01-12 23:22:07 -0600

    Time: 57 ms, Memory: 42 MB - LeetHub

--- minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
+++ minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -3,26 +3,37 @@
  * @return {number}
  */
 var minStartValue = function(nums) {
-    let currentNum = 1;
-    let runningTotal = currentNum;
+    //initialize startValue to 1, since the startValue can never be below 1
+    let startValue = 1;
     
-    while(currentNum <= 10000){
+    /*initialize the runningTotal to be the value of startValue, as this is the variable we will use to keep
+    track of whether or not this runningTotal ever dips below 1 when having all of the elements of nums added
+    to it*/
+    let runningTotal = startValue;
+    
+    /*we start a while loop that checks if currentNum runs while startValue is less than or equal to
+    10k(arbitrary number that passed test cases since problem didnt explicitly give me a limit)*/
+    while(startValue <= 10000){
+        
+        /*create for loop that adds our runningTotal variable to every element in the nums array, and every
+        iteration checks if the runningTotal ever is below 1, if it is we break free from the for loop*/
         for(let i = 0; i < nums.length; i++){
             runningTotal += nums[i];
-            // console.log("runningTotal: ", runningTotal)
             
             if(runningTotal < 1){
-                // console.log("ALERT it hit less than 1, breaking for loop");
-                break
+                break;
             }
         }
         
+        /*here we have a simple if/else conditionial statement that checks if the runningTotal after being done
+        with the for loop above is still above 0, if so we will return the current startValue. If not, in the
+        else block we will first increment the startValue variable by 1 and then initialize the runningTotal
+        variable to the new value of startValue*/
         if(runningTotal > 0){
-            return currentNum
+            return startValue;
         }else{
-            currentNum++;
-            // console.log("currentNum now: ", currentNum)
-            runningTotal = currentNum;
+            startValue++;
+            runningTotal = startValue;
         }
     }
 };

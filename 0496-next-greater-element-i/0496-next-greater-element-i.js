440b8c67aa2ea39b27e0e8c95670993011089f83
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-03-19 22:02:45 -0500

    Time: 64 ms (46.34%), Space: 52.3 MB (30.29%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let rightNumGreaterThanMap = new Map();
+    
+    for(let i = 0; i < nums1.length; i++){
+        if(!rightNumGreaterThanMap.has(nums1[i])){
+            rightNumGreaterThanMap.set(nums1[i], -1);
+        }
+    }
+    
+    for(let i = 0; i < nums2.length; i++){
+        if(rightNumGreaterThanMap.has(nums2[i])){
+            for(let j  = i; j < nums2.length; j++){
+                if(nums2[i] < nums2[j]){
+                    rightNumGreaterThanMap.set(nums2[i], nums2[j]);
+                    break;
+                }
+            }
+        }
+    }
+    
+    console.log(rightNumGreaterThanMap);
+    
+    let answerArr = []
+    
+    for(let [key, value] of rightNumGreaterThanMap){
+        answerArr.push(value);
+    }
+    
+    return answerArr;
+};

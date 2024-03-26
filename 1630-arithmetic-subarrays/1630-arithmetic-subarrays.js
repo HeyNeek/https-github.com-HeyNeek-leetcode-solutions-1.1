4cc8429c2834a1c8b14272f328ec5803fa05c53a
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-03-26 13:17:33 -0500

    Time: 120 ms (55.75%), Space: 56.3 MB (71.26%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
@@ -7,31 +7,22 @@
 var checkArithmeticSubarrays = function(nums, l, r) {
     let subarrayMap = new Map();
     
-    console.log("left: ", l);
-    console.log("right: ", r);
-    
     for(let i = 0; i < l.length; i++){
         subarrayMap.set(i, nums.slice(l[i], r[i] + 1));
     }
     
-    console.log(subarrayMap);
     let answerArr = [];
     
     for(let [key, value] of subarrayMap){
         const descendingOrder = value.sort((a,b) => b-a);
         const commonDiff = descendingOrder[0] - descendingOrder[1];
-        // console.log("sorted array: ", descendingOrder);
-        // console.log("commonDiff: ", commonDiff);
         let isArithmetic = true;
         for(let i = 0; i < descendingOrder.length; i++){
-            console.log("currentDiff: ", descendingOrder[i] - descendingOrder[i + 1])
             if(isNaN(descendingOrder[i] - descendingOrder[i + 1])){
-                console.log("made it in the NaN trigger")
                 break;
             }
             
             if(descendingOrder[i] - descendingOrder[i + 1] !== commonDiff){
-                console.log("made it in the false trigger")
                 isArithmetic = false;
                 break;
             }

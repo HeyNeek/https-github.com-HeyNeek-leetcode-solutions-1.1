fb03c7cab2d2f6f1ad787c61babee042c61e3a4a
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-04-14 23:19:01 -0500

    Time: 162 ms (23.40%), Space: 59.8 MB (34.94%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
@@ -4,13 +4,9 @@
  */
 var findNonMinOrMax = function(nums) {
     let sortedArr = nums.sort((a,b) => a-b);
-    // console.log(sortedArr)
     
     let min = sortedArr[0];
     let max = sortedArr[sortedArr.length - 1];
-    
-    // console.log(min)
-    // console.log(max)
     
     for(let i = 0; i < sortedArr.length; i++){
         if(sortedArr[i] !== min && sortedArr[i] !== max){

f68d05bdd2f0f90df903689e7a81a565bfbbdda7
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-04-14 23:09:44 -0500

    Time: 65 ms (62.11%), Space: 52 MB (62.74%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -3,15 +3,7 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let answerArr = [];
-    
-    for(let i = 0; i < nums.length; i++){
-        let splitNum = String(nums[i]).split("");
-        
-        for(let j = 0; j < splitNum.length; j++){
-            answerArr.push(Number(splitNum[j]));
-        }
-    }
-    
-    return answerArr;
+    let string = nums.toString().replaceAll(',',"")
+    let arr = Array.from(string,Number)
+    return arr
 };

ce16444dbbc524d68e9877fbdb074c77ef1cb314
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-04-17 16:28:26 -0500

    Time: 57 ms (28.06%), Space: 48.8 MB (80.11%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+//     let lesserThanBeginning = false;
+//     let greaterThanEnd = false;
+    
+    if(!nums.includes(target)){
+        if(target < nums[0]){
+            return 0;    
+        }
+        
+        if(target > nums[nums.length - 1]){
+            return nums.length;
+        }
+        
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i - 1] && nums[i]){
+                 if(target > nums[i - 1] && target < nums[i]){
+                     return i;
+                 }
+            }
+        }
+    }else{
+        return nums.indexOf(target);
+    }
+};

a46a8ce063186e8c10f39064ada44d9cbdfc6f31
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-04-25 15:32:19 -0500

    Time: 50 ms (72.88%), Space: 51.2 MB (5.08%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
@@ -6,26 +6,22 @@ var findSubarrays = function(nums) {
     let subArrayTotalsMap = new Map();
     
     for(let i = 0; i < nums.length; i++){
+        let currentSubarraySum = nums[i] + nums[i + 1];
         
-            let currentSubarraySum = nums[i] + nums[i + 1]
-            
-            if(!subArrayTotalsMap.has(currentSubarraySum)){
-                subArrayTotalsMap.set(currentSubarraySum, 1);
-            }else{
-                subArrayTotalsMap.set(currentSubarraySum, subArrayTotalsMap.get(currentSubarraySum) + 1);
-            }
-        
+        if(!subArrayTotalsMap.has(currentSubarraySum)){
+            subArrayTotalsMap.set(currentSubarraySum, 1);
+        }else{
+            subArrayTotalsMap.set(currentSubarraySum, subArrayTotalsMap.get(currentSubarraySum) + 1);
+        }
     }
-    
-    console.log(subArrayTotalsMap)
-    
+        
     for(let [key, value] of subArrayTotalsMap){
         if(value > 1){
-            return true
+            return true;
         }
     }
     
    
     
-    return false
+    return false;
 };

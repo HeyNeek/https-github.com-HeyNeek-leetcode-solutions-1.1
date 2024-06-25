352e3ce3d97354bd8fda925fc1a3636140818220
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2024-06-24 23:17:08 -0700

    Time: 65 ms (34.65%), Space: 51.4 MB (22.38%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.js
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.js
@@ -7,11 +7,5 @@ var addedInteger = function(nums1, nums2) {
     let sortedNums1 = nums1.sort((a,b) => a-b);
     let sortedNums2 = nums2.sort((a,b) => a-b);
     
-    console.log(sortedNums1);
-    console.log(sortedNums2);
-    
-    
-        return sortedNums2[0] - sortedNums1[0];
-   
-
+    return sortedNums2[0] - sortedNums1[0];
 };
